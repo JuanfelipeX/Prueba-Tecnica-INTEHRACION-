5c39a2a9a3c4de7287a3d86d99be44c558ca30dc
Juan Felipe Jimenez P. <pipesjimenez6@gmail.com>
2024-04-10 12:33:13 -0500

    prueba tecnica

---
+++ src/main/java/com/messageservice/demo/model/Device.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.messageservice.demo.model;
+
+/**
+ *
+ * @author pipes
+ */
+public class Device {
+
+    private String id;
+    private String[] message;
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String[] getMessage() {
+        return message;
+    }
+
+    public void setMessage(String[] message) {
+        this.message = message;
+    }
+
+}
